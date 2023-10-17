d3eb6c5e962c1b2d30a8260481285122a628e644
Eleazar Z <63356125+EZP99@users.noreply.github.com>
2023-10-17 11:19:18 -0600

    Add files via upload

---
+++ html/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El Caminante</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <article class="logo">
+                <a href="#"> <img src="assets/logo.png" alt="logo"></a>
+            </article>
+            <article>
+                <ul class="barra-nav">
+                    <li class="menu-horizontal"><a href="#">Inicio</a></li>
+                    <li class="menu-horizontal"><a href="#">Tienda</a>
+                        <ul class="menu-vertical">
+                            <li><a href="">Hombre</a></li>
+                            <li><a href="">Mujer</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-horizontal"><a href="#">Nosotros</a></li>
+                    <li class="menu-horizontal"><a href="#">Contáctanos</a></li>
+                    <li class="menu-horizontal"><a href="#"><img src="assets/carrito.svg" alt="carrito"></a></li>
+                </ul>
+            </article>
+            <article class="menu-celular">
+                <div class="menu-ham"><img src="assets/menu.svg" alt="menu-ham">
+                    <ul class="menu">
+                        <li><a href="" >Inicio</a></li>
+                        <li><a href="" >Tienda</a>
+                            <ul class="menu-vertical">
+                                <li><a href="">Hombre</a></li>
+                                <li><a href="">Mujer</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Nosotros</a></li>
+                        <li><a href="#"><img src="assets/carrito-celu.svg" alt="carrito"></a></li>
+                    </ul>
+                </div>
+            </article>
+        </nav>
+    </header>
