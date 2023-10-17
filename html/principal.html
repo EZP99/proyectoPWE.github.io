88c58b54b8ac3278d0326d7f2a43324370381754
Eleazar Z <63356125+EZP99@users.noreply.github.com>
2023-10-17 12:30:21 -0600

    Add files via upload

---
+++ html/principal.html
@@ -0,0 +1,13 @@
+<main>
+    <section>
+        <article class="video">
+            <div class="fondo"></div>
+            <div class="titulo-video">
+                <h1>El Caminante</h1>
+            </div>
+            <video onloadedmetadata="this.muted=true" autoplay loop preload="auto">
+                <source src="assets/shoes_-_11284 (Original).mp4">
+            </video>
+        </article>
+    </section>
+</main>
---
+++ script.js
@@ -0,0 +1,46 @@
+// Función para cargar y mostrar el encabezado
+function cargarEncabezado() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "./html/header.html", true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("header").innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.send();
+}
+
+// Función para cargar y mostrar el contenido principal
+function cargarContenidoPrincipal() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "./html/principal.html", true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("main").innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.send();
+}
+
+// Función para cargar y mostrar el pie de página
+function cargarPieDePagina() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "./html/footer.html", true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("footer").innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.send();
+}
+
+// Llama a las funciones para cargar el encabezado, el contenido principal y el pie de página
+cargarEncabezado();
+cargarContenidoPrincipal();
+cargarPieDePagina();
